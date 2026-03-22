77020ffc566015e487d0efee9950429227537082
Romaltix <romaltix@gmail.com>
2026-03-22 22:59:57 +0300

    ITreolanClient

---
+++ Suppliers-API/Integrations/Suppliers/Treolan/ITreolanClient.cs
@@ -0,0 +1,14 @@
+﻿namespace Suppliers_API.Integrations.Suppliers.Treolan
+{
+	/// <summary>
+	/// Контракт клиента для работы с API Treolan.
+	/// Определяет, какие операции мы можем выполнять.
+	/// </summary>
+	public interface ITreolanClient
+	{
+		/// <summary>
+		/// Получить токен авторизации Treolan.
+		/// </summary>
+		Task<string> GetTokenAsync(CancellationToken cancellationToken = default);
+	}
+}
--- Suppliers-API/obj/Debug/net10.0/Suppliers-API.MvcApplicationPartsAssemblyInfo.cs
+++ Suppliers-API/obj/Debug/net10.0/Suppliers-API.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Suppliers-API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1cfbb720575044630df6ce76b943248899bc54")]
-[assembly: System.Reflection.AssemblyProductAttribute("Suppliers-API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Suppliers-API")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
